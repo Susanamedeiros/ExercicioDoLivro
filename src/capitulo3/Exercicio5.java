40fe725ad8461eb3bb7b34758bb14046502c1398
Susanamedeiros <susiweb2009@gmail.com>
2013-08-06 16:54:38 -0300

    Conclui o exercicio 5

--- src/capitulo3/Exercicio5.java
+++ src/capitulo3/Exercicio5.java
@@ -1,5 +1,14 @@
 package capitulo3;
 
-public class Exercicio5 {
+import java.util.Scanner;
 
+public class Exercicio5 {
+	public static void main (String [] args){
+	Scanner scanner = new Scanner (System.in);
+	float preco = scanner.nextFloat();
+	float precoMeio = (preco / 100) * 10;
+	float precoFim = preco - precoMeio;
+	System.out.println("O novo preco eh: " + precoFim);
+	scanner.close();
+	}
 }
