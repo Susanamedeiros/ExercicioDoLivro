25b1158306f49f24069aa3b7e715cf7d1e76abfd
Susanamedeiros <susiweb2009@gmail.com>
2013-08-06 15:30:30 -0300

    Conclui o primeiro exercicio

--- src/capitulo3/Exercicio1.java
+++ src/capitulo3/Exercicio1.java
@@ -1,7 +1,16 @@
 package capitulo3;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
 	public static void main (String [] args){
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite um numero: ");
+		int num1 = scanner.nextInt();
+		System.out.println("Digite outro numero: ");
+		int num2 = scanner.nextInt();
+		int sub = num1 - num2;
+		System.out.println("A subtracao delas eh: " + sub);
+		scanner.close();
 	}
 }
