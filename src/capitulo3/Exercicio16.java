6c6be3c3cc37dd86b91ef1da8d2c4b630b0986f5
Susanamedeiros <susiweb2009@gmail.com>
2013-08-13 16:38:35 -0300

    Conclui, finalmente, o exercicio 16

--- src/capitulo3/Exercicio16.java
+++ src/capitulo3/Exercicio16.java
@@ -6,15 +6,13 @@ import java.lang.Math;
 public class Exercicio16 {
 	public static void main (String[] args){
 		Scanner scanner = new Scanner(System.in);
-		/*double cat1, cat2, hip;*/
-		float teste;
-		/*System.out.println("Digite os dois catetos: ");
+		double cat1, cat2, hip;
+		System.out.println("Digite os dois catetos: ");
 		cat1 = scanner.nextDouble();
 		cat2 = scanner.nextDouble();
 		hip = Math.pow(cat1, 2.0) + Math.pow(cat2, 2.0);
-		Math.pow(hip, 2);*/
-		teste = Math.pow(2, 3);
-		System.out.println(teste);
+		Math.pow(hip, 2);
+		System.out.println("A hipotenusa é: " + hip);
 		scanner.close();
 	}
 }
