36af227953b98a4d71b36a6bf615513cafd8ff89
Susanamedeiros <susiweb2009@gmail.com>
2013-08-13 15:42:47 -0300

    Conclui o exercicio 10

--- src/capitulo3/Exercicio10.java
+++ src/capitulo3/Exercicio10.java
@@ -1,5 +1,15 @@
 package capitulo3;
 
-public class Exercicio10 {
+import java.util.Scanner;
 
+public class Exercicio10 {
+	public static void main (String[] args){
+		Scanner scanner = new Scanner(System.in);
+		float A, lado;
+		System.out.println("Digite o lado: ");
+		lado = scanner.nextFloat();
+		A = lado * lado;
+		System.out.println("A área do quadrado é: " + A);
+		scanner.close();
+	}
 }
